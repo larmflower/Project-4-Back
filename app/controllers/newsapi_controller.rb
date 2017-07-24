f315442e524ba4af511233fb675659d33e144914
larmflower <lnarosetta@gmail.com>
2017-07-24 11:29:23 +0100

    Articles display in home page on click, filtering can be done on array of articles

--- app/controllers/newsapi_controller.rb
+++ app/controllers/newsapi_controller.rb
@@ -1,15 +1,20 @@
 class NewsapiController < ApplicationController
-    skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!
   def news
-    p  params["news"]
-    newsType = HTTParty.get("https://newsapi.org/v1/articles?source=#{params[:news]}&apiKey=59126808b2724541acd1c195d82b52a0", {
-        query: {
-          units: "si"
-        },
-        headers: { "Accept" => "application/json" }
-    }).parsed_response
-    p newsType
-    render json: newsType, status: :ok
-
+    articles = HTTParty.get("https://newsapi.org/v1/articles?source=#{params[:news]}&apiKey=59126808b2724541acd1c195d82b52a0", {
+      query: {
+        units: "si"
+      },
+      headers: { "Accept" => "application/json" }
+      }).parsed_response
+      render json: articles, status: :ok
   end
 end
+
+  # e['Products'].each do |product|
+  #do something with product['Name']
+  # end
+  # <% @foods.each do |food| %>
+  #    <%= food.name %>
+  #    <%= food.calorie %>
+  # <% end %>
