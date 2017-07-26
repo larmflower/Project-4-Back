649ec0ad7099fde52d5f36b5ba78560e7d3d66da
Alexandria Okamoto <alexandriamhko@gmail.com>
2017-07-26 11:42:11 +0100

    filtering

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  scope :of_friends, -> (friends) {where user_id: friends }
+  scope :of_me, -> (current_user) {where user_id: current_user }
 end
