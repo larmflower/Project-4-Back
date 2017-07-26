422865bd2b792c0580ea9d37a400007b6232fb9e
larmflower <lnarosetta@gmail.com>
2017-07-26 11:44:08 +0100

    commit

--- app/controllers/newsapi_controller.rb
+++ app/controllers/newsapi_controller.rb
@@ -1,20 +1,15 @@
 class NewsapiController < ApplicationController
-  skip_before_action :authenticate_user!
+    skip_before_action :authenticate_user!
   def news
+    # p  params["news"]
     articles = HTTParty.get("https://newsapi.org/v1/articles?source=#{params[:news]}&apiKey=59126808b2724541acd1c195d82b52a0", {
-      query: {
-        units: "si"
-      },
-      headers: { "Accept" => "application/json" }
-      }).parsed_response
-      render json: articles, status: :ok
+        query: {
+          units: "si"
+        },
+        headers: { "Accept" => "application/json" }
+    }).parsed_response
+    p articles
+    render json: articles, status: :ok
+
   end
 end
-
-  # e['Products'].each do |product|
-  #do something with product['Name']
-  # end
-  # <% @foods.each do |food| %>
-  #    <%= food.name %>
-  #    <%= food.calorie %>
-  # <% end %>
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,3 @@
 class Post < ApplicationRecord
-  mount_uploader :image, ImageUploader
   belongs_to :user
-  has_many :comments, dependent: :destroy
 end
